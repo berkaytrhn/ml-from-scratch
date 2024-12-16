softmax and derv. impl, onehot encoder impl, cross entropy loss impl, not working due to a bug yet

ml_lib/linear/logistic_regression.py
@@ -133,9 +133,16 @@ class LogisticRegression(LinearModel):
                 reg_grads = self.regularization.backward(self.weights) if regularize else 0
                 # loss backward
                 loss_intermediates: np.ndarray = self.loss.backward(y, y_pred)
-                # caches the activation value on __call__
-                activation_intermediate: np.float64 = self.activation.backward()
                 
+                # TODO: Fix bug for following backprop of softmax activation
+                # caches the activation value on __call__
+                activation_intermediate = None
+                if isinstance(self.activation, Softmax):  
+                    # returns (N, C, C) jacobian matrix
+                    activation_intermediate = self.activation.backward(loss_intermediates)
+                else:
+                    activation_intermediate = self.activation.backward()
+                    
                 # performing optimnization
                 self._optimize(x, (loss_intermediates, activation_intermediate, reg_grads))
                 

ml_lib/metrics/losses.py
@@ -19,6 +19,9 @@ class Loss(ABC):
         y_pred:np.ndarray
     ) -> Tuple[np.float64, np.float64]:
         """Loss Backward Abstract Method"""
+    
+    def clipping_preds(self, data, epsilon=1e-15):
+        return np.clip(data, epsilon, 1 - epsilon)
 
 
 
@@ -63,9 +66,6 @@ class BCELoss(Loss):
     """
     Basic Implementation of Binary Cross Entropy Losss
     """
-    def __clipping_preds(self, data, epsilon=1e-15):
-        return np.clip(data, epsilon, 1 - epsilon)
-    
     def __call__(
         self,
         y_true:np.ndarray,
@@ -73,11 +73,7 @@ class BCELoss(Loss):
     ) -> np.float64:
         """BCE Loss Calculation """
         
-        y_pred = self.__clipping_preds(y_pred)
-        # print("neg_log_0 : ", neg_log_0.shape)
-        # print("neg_log_1 : ", neg_log_1.shape)
-        # print((neg_log_0+neg_log_1).shape)
-        # print(-np.mean(neg_log_0+neg_log_1))
+        y_pred = self.clipping_preds(y_pred)
         return -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
 
     
@@ -89,8 +85,35 @@ class BCELoss(Loss):
     ) -> np.ndarray:
         """Loss Backward """
 
-        y_pred = self.__clipping_preds(y_pred)
+        y_pred = self.clipping_preds(y_pred)
 
         # Calculate derivative of "loss" wrt "y_pred"
         return (y_pred - y_true) / (y_pred * (1 - y_pred))
+
+
+class CrossEntropyLoss(Loss):
     
+    def  __call__(
+        self,
+        y_true:np.ndarray,
+        y_pred:np.ndarray
+    ) -> np.float64:
+        """
+        Cross Entropy Loss Calculation
+        y_true, y_pred: (N,C) matrices(assumed to be one hot encoded)
+        """
+        assert (y_true.ndim==2 and y_pred.ndim==2), "Ground truth values are expected as one hot encoded!!"
+        
+        loss = -np.sum(y_true * np.log(y_pred), axis=1)
+        return np.mean(loss)
+    
+    def backward(
+        self, 
+        y_true:np.ndarray, 
+        y_pred:np.ndarray
+    ) -> np.ndarray:
+        """
+        Derivative of Cross Entropy Loss
+        """
+        number_of_samples = y_true.shape[0]
+        return (y_pred - y_true) / number_of_samples

ml_lib/utilities/activations.py
@@ -38,28 +38,28 @@ class Softmax(Activation):
         self.cache=None
     
     def __call__(self, z):
-        # subtract from max to avoid exponential explosion to infinity, now exp result will be max 1
         exps = np.exp(z - np.max(z, axis=1, keepdims=True)) 
         
         self.cache = exps / np.sum(exps, axis=1, keepdims=True)
-        # max along axis=1(check among columns, horizontal direction) and shape[0] same with inputs
+        
+        print("Softmax output shape:", self.cache.shape)
+        
         return self.cache
-    
-    def backward(self):
+        
+    def backward(self, d_out: np.ndarray):
         """
         Since softmax depends on number_of_class values,
         we should calculate partial derivative wrt all individually in a loop
         
         d_out: Derivative of loss wrt softmax output -> (N,C) = (number_of_samples, number_of_classes)
         """
-        
         N, C = self.cache.shape
         
         # for a case like receiving next derivatives and calculate  
-        # d_logits = np.zeros_like(d_out)
+        d_logits = np.zeros_like(d_out)
         
         # for a case like calculating immediate derivatives and later connect them
-        jacobians = np.zeros((N, C, C))
+        # jacobians = np.zeros((N, C, C))
         
         # Loop in all datapoints
         for i in range(N):  
@@ -73,11 +73,11 @@ class Softmax(Activation):
             jacobian = np.diagflat(softmax_output) - np.dot(softmax_output, softmax_output.T)  
             
             # Computing the gradient for the i-th data point for complete usage
-            # d_logits[i] = np.dot(jacobian, d_out[i])
+            d_logits[i] = np.dot(jacobian, d_out[i].reshape(1, -1))
             
             # Computing jacobian matrices for all samples for immediate calculation implementation
             # Shape : (N, C, C)
-            jacobians[i] = jacobian
+            # jacobians[i] = jacobian
         
-        return jacobians
+        return d_logits
     